ed4a3db60e77c58978e9e1cee1ba40c2a29fe885
Vayras <89837102+Vayras@users.noreply.github.com>
2023-10-10 23:06:10 +0530

    logo final

--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -1,5 +1,5 @@
 import Header from "@/components/Header";
-
+import Footer from "@/components/jscomponents/Footer";
 function Product({searchParams}:any) {
 
   console.log(searchParams , "params")
@@ -9,6 +9,7 @@ function Product({searchParams}:any) {
       <h1 className="text-black">{searchParams.title}</h1>
       <h1 className="text-black">{searchParams.description}</h1>
       <img src={searchParams.imageUrl} alt="" />
+      <Footer/>
   </div>
   );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,34 +5,42 @@ import { FaTimes } from "react-icons/fa";
 import { FaBarsStaggered } from 'react-icons/fa6';
 import Link from 'next/link';
 import Hero from './Hero';
-
+import logo from '../app/assets/logo.svg'
+import Image from 'next/image';
+import logo3 from "../app/assets/logo3.png"
+import logo4 from "../app/assets/logo4.png"
 
 const Header = () => {
 
     const [Open, setOpen] = useState(false);
     const handleMenu = () => {
-        setOpen( (prev) => !prev);
+        setOpen((prev) => !prev);
     }
 
-    const toProducts=() =>{
-        const target=document.getElementById("Products");
-        if(target){
-            target.scrollIntoView({behavior:'smooth'});
+    const toProducts = () => {
+        const target = document.getElementById("Products");
+        if (target) {
+            target.scrollIntoView({ behavior: 'smooth' });
         }
     };
 
-    const toFooter=() => {
+    const toFooter = () => {
         const target = document.getElementById("Footer");
-        if(target){
-            target.scrollIntoView({behavior:"smooth"});
+        if (target) {
+            target.scrollIntoView({ behavior: "smooth" });
         }
     };
     return (
         <div>
             <div className='flex w-full min-h-[5.5rem] justify-between items-center'>
-                <div className='flex items-center'>
-                    <img src='src\app\assets\logo-black.png' alt='Logo' className='h-[3rem] sm:h-[4rem]'></img>
-                    <p className='text-[#000] font-serif text-[1.8rem] font-bold leading-normal'>Arihant Art Exports</p>
+                <div className='flex items-center justify-center '>
+                        <Image
+                            className='h-[4rem] w-[4rem] ml-4 mr-2'
+                            priority
+                            src={logo4}
+                            alt="Follow us on Twitter"
+                        />
+                        <p className='text-[#000] font-serif text-[1.8rem] font-bold leading-normal'>Arihant Art Exports</p>
                 </div>
 
                 <div className=''>
@@ -55,15 +63,15 @@ const Header = () => {
                     </button>
                 </div>
             </div>
-            {Open? (
+            {Open ? (
                 <div className='md:hidden'>
                     <div className='px-2 pt-2 pb-3 space-y-1 sm:px-3'>
-                    <ul className='text-[#000] px-3 py-2 rounded-md text-base mx-auto flex flex-col text-center text-[1rem] leading-normal font-semibold font-sans gap-4'>
-                        <li className='hover:underline'><Link href="/home">Home</Link></li>
-                        <li className='hover:underline'onClick={toProducts}>Shop</li>
-                        <li className='hover:underline'>About</li>
-                        <li className='hover:underline' onClick={toFooter} >Contact Us</li>
-                    </ul>
+                        <ul className='text-[#000] px-3 py-2 rounded-md text-base mx-auto flex flex-col text-center text-[1rem] leading-normal font-semibold font-sans gap-4'>
+                            <li className='hover:underline'><Link href="/home">Home</Link></li>
+                            <li className='hover:underline' onClick={toProducts}>Shop</li>
+                            <li className='hover:underline'>About</li>
+                            <li className='hover:underline' onClick={toFooter} >Contact Us</li>
+                        </ul>
                     </div>
                 </div>
             ) : null}
